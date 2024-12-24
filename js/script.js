a40a2329d660def5c992865ddd2815dedef30f7c
Victor Estevam Pereira <victorestevamp@gmail.com>
2024-12-24 10:59:34 -0300

    otimização do código

--- js/script.js
+++ js/script.js
@@ -8,18 +8,32 @@ generatorButton.addEventListener('click', () => {
   getApiData()
 })
 
-function getApiData() {
-  fetch(apiLink)
-    .then(response => response.json())
-    .then(data => {
-      hideLoadingMessage()
+async function getApiData() {
+  try {
+    const apiResponse = await fetch(apiLink);
+    /* 
+      A propriedade .ok é responsável por verificar se a resposta (Response) foi feita com sucesso ou não, 
+  
+      nesse caso estamos negando a resposta, ou seja, se a resposta não foi feita com sucesso, retornamos um erro.
+    
+      Ela está presente no objeto Response, que é retornado pela função fetch. 
+    */
+      if (!apiResponse.ok){
+        throw new Error("Ocorreu um erro ao tentar buscar as informações da API");
+      }
 
-      const adviceIdElement = document.querySelector('#advice-id')
-      const adviceTextElement = document.querySelector('#advice-text')
+    const data = await apiResponse.json()
+    hideLoadingMessage()
 
-      adviceIdElement.innerText = `Advice #${data.slip.id}`
-      adviceTextElement.innerText = `"${data.slip.advice}"`
-    })
+    const adviceIdElement = document.querySelector('#advice-id')
+    const adviceTextElement = document.querySelector('#advice-text')
+
+    adviceIdElement.innerText = `Advice #${data.slip.id}`
+    adviceTextElement.innerText = `"${data.slip.advice}"`
+  } catch (error) {
+    console.error("Erro ao tentar buscar as informações da API", error);
+  }
+
 }
 
 function hideLoadingMessage() {
